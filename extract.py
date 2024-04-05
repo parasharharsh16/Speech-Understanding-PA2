9423dd2fc9e33299cb7c941ceebb64bd55f1435f
harshp <parasharharsh16@gmail.com>
2024-04-05 14:26:03 +0530

    eer calculation for 3 pretrained model on voxcelb-1H dataset

--- extract.py
+++ extract.py
@@ -1,7 +1,4 @@
-from VOXCELEB1_dataset import VoxCeleb1
-
 from torchaudio.datasets import voxceleb1
 voxceleb1_dataset = voxceleb1.VoxCeleb1(root="dataset", download=True)
 
-
-print("test")
+print("dataset downloaded")
